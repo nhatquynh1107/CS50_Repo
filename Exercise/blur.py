560301295008eaf2745570bca783e73a7110f715
Nhat Quynh <nhatquynhnguyen1107@gmail.com>
2025-12-03 02:33:32 +0000

    update code

--- Exercise/blur.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image, ImageFilter
-
-before = Image.open("")
-after = before.filter(ImageFilter.BoxBlur(1))
-after.save("")
--- Exercise/phonebook.c
+++
@@ -1,6 +0,0 @@
-#include <cs50.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
-
-int main (void)
